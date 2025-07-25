155492e64ec6e3964ad3ab86f347127dab5f6ba9
kebab-casee <bombelatz@gmail.com>
2025-07-25 12:40:40 +0800

    size,number input

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
-<script setup></script>
+<template>
+  <p>
+    Input box color:
+    <input type="text" v-model="color" />
+  </p>
+  <p>
+    Input box size:
+    <input type="number" v-model="size" />
+  </p>
+  <div :style="{ width: size + 'px', height: size + 'px', backgroundColor: color }"></div>
+</template>
 
-<template></template>
+<script setup>
+import { ref } from 'vue'
+
+const color = ref('')
+const size = ref(0)
+</script>
 
 <style scoped></style>
