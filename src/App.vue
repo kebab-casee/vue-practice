714772b9ebacb2ec37c46b8ea9b68f745778af40
kebab-casee <bombelatz@gmail.com>
2025-07-26 22:52:51 +0800

    add Bootstrap stylesheet to App.vue and initialize movieList

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
- 
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+    crossorigin="anonymous"
+  />
 </template>
 
 <script setup>
 import { ref, computed, reactive, watch } from 'vue'
 
+const movieList = [
+    {id: 1, name: ''}
+]
 
 </script>
 
